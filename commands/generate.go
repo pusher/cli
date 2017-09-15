e92fbee95af39bc8607a9faa374c765bd2859490
Jim Fisher <jameshfisher@gmail.com>
2017-09-15 15:16:59 +0100

    generate web

--- commands/generate.go
+++ commands/generate.go
@@ -9,7 +9,18 @@ var Generate = &cobra.Command{
 	Use:   "generate",
 	Short: "Generate a starter project which uses a Pusher app",
 	Args:  cobra.NoArgs,
+}
+
+var GenerateWeb = &cobra.Command{
+	Use:   "web",
+	Short: "Generate a starter web project which uses a Pusher app",
+	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("TODO Generating something")
+		fmt.Println("TODO Generating a web project")
 	},
+}
+
+func init() {
+	Generate.PersistentFlags().StringVar(&appId, "app-id", "", "Pusher App ID")
+	Generate.AddCommand(GenerateWeb)
 }
