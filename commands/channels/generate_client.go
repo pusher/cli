d3c22e59ce47bb04b2352ffe0e346d8257119d68
Hugo Vieira <hugo.andre.vieira@gmail.com>
2017-10-25 16:18:00 +0100

    Changed product name. DDN --> Channels

--- commands/channels/generate_client.go
+++ commands/channels/generate_client.go
@@ -1,4 +1,4 @@
-package ddn
+package channels
 
 import (
 	"fmt"
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/pusher/pusher-cli/commands/auth"
-	"github.com/pusher/pusher-cli/commands/ddn"
+	"github.com/pusher/pusher-cli/commands/channels"
 	"github.com/spf13/cobra"
 )
 
@@ -10,11 +10,11 @@ func main() {
 	var rootCmd = &cobra.Command{Use: "pusher"}
 	var Apps = &cobra.Command{Use: "apps",
 		Short: "Manage your Pusher Apps"}
-	Apps.AddCommand(ddn.Apps, ddn.Tokens, ddn.Subscribe, ddn.Trigger)
+	Apps.AddCommand(channels.Apps, channels.Tokens, channels.Subscribe, channels.Trigger)
 
 	var Generate = &cobra.Command{Use: "generate",
 		Short: "Generate a Pusher client, server, or Authorisation server"}
-	Generate.AddCommand(ddn.GenerateClient, ddn.GenerateServer, ddn.LocalAuthServer)
+	Generate.AddCommand(channels.GenerateClient, channels.GenerateServer, channels.LocalAuthServer)
 	rootCmd.AddCommand(Generate, Apps)
 	rootCmd.AddCommand(auth.Login, auth.Logout)
 	rootCmd.Execute()
