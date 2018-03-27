755f4698e054ad62f4cf80728a61979fea1b6626
Will Sewell <me@willsewell.com>
2018-03-27 15:48:08 +0100

    Create .config dir if it does not exist

--- config/config.go
+++ config/config.go
@@ -28,12 +28,23 @@ func getUserHomeDir() string {
 	return usr.HomeDir
 }
 
+func getConfigDir() string {
+	return path.Join(getUserHomeDir(), ".config2")
+}
+
 func getConfigPath() string {
-	return path.Join(getUserHomeDir(), ".config/pusher.json")
+	return path.Join(getConfigDir(), "pusher.json")
 }
 
 //Init sets the config files location and attempts to read it in.
 func Init() {
+	if _, err := os.Stat(getConfigDir()); os.IsNotExist(err) {
+		err = os.Mkdir(getConfigDir(), os.ModeDir|0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	if _, err := os.Stat(getConfigPath()); os.IsNotExist(err) {
 		err = ioutil.WriteFile(getConfigPath(), []byte("{}"), 0600)
 		if err != nil {
